0983d636289a8297637daae2f3a2737f96fbf2f0
kawsar0277 <kawsar1252005@gmail.com>
2025-06-17 18:46:52 +0600

    class-33 added in repository

---
+++ script.js
@@ -0,0 +1,75 @@
+ // console.log('Javascript Intro');
+
+        // console.log(45+50);
+
+        // x = 45;
+        // y = 50;
+
+        // z = x+y;
+
+        // console.log(z);
+
+
+        // var rahim = 200;
+        // var karim = 300;
+
+        // var total = rahim+karim;
+
+        // console.log(total);
+
+        // let rahim = 200;
+        // let karim = 300;
+
+        // let total = rahim+karim;
+
+        // console.log(total);
+
+
+        // var x = 10;
+        // var y = 50;
+        // var x = 57;
+
+        // var z = x+y;
+
+        // console.log(z);
+
+
+        // let k = 10;
+        // var l = 30;
+        
+        // let m = k+l;
+
+        // console.log(m);
+
+        // const pie = 3.1416;
+
+        // var price = 200;
+
+
+        // price = 210;
+
+        // let sum = 300;
+
+        // sum = 400;
+
+        // // pie = 400;
+
+        // console.log(pie);
+
+
+
+        let totalMoney = 1000;
+
+        let totalEgg = 5;
+        let eachEggsPrice = 11;
+        let totalChocolate = 2;
+        let eachChocolatePrice = 40;
+
+        let eggPrice = totalEgg * eachEggsPrice;
+        let chocolatePrice = totalChocolate * eachChocolatePrice;
+
+        let totalCost = eggPrice+chocolatePrice;
+        
+        let remainingMoney = totalMoney-totalCost;
+
+        console.log("Remainig Money ="+remainingMoney);
